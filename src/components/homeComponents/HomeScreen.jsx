f6c02551df0eddefa48ba9b0e1760dbf06a3cec7
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-10-25 22:09:31 -0600

    here we go

---
+++ src/components/homeComponents/HomeScreen.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import AdBanner from './AdBanner'
+import axios from 'axios'
+
+const HomeScreen = () => {  
+  const [recipe, setRecipes] = useState([])
+
+  const getRecipes = () => {
+    axios
+        .get("https://recipes.devmountain.com/recipes")
+        .then((res) => {
+            setRecipes(res.data)
+            console.log(res.data)
+        })
+}
+
+useEffect(() => {
+    getRecipes()
+},[])
+
+  return (
+    <div>
+      <AdBanner />
+      {/* Much code from Part 2 will be placed around here. Do your best! */}
+    </div>
+  )
+}
+
+export default HomeScreen
